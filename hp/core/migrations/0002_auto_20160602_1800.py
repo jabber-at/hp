c50670df3046211d219ad0e92d07bd5be9dfe68f
Mathias Ertl <mati@er.tl>
2016-06-02 23:08:40 +0200

    set a better default

--- hp/core/migrations/0002_auto_20160602_1800.py
+++ hp/core/migrations/0002_auto_20160602_1800.py
@@ -41,7 +41,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='page',
             name='author',
-            field=models.ForeignKey(default='', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default='1', on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
         migrations.AddField(
