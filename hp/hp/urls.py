0ba1b3f995925e834ff1a505e5078c5b77af3cbf
Mathias Ertl <mati@er.tl>
2016-06-09 11:45:56 +0200

    add default url view

--- hp/hp/urls.py
+++ hp/hp/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -20,5 +21,6 @@ from blog.views import BlogPostListView
 
 urlpatterns = [
     url(r'^$', BlogPostListView.as_view(), name='root'),
+    url(r'^', include('core.urls')),
     url(r'^admin/', admin.site.urls),
 ]
