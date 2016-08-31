6d1800ac1b0f11728302daec123bbe306784a8f2
Mathias Ertl <mati@er.tl>
2016-08-31 18:25:08 +0200

    add path templatetag

--- hp/core/templatetags/render.py
+++ hp/core/templatetags/render.py
@@ -13,14 +13,26 @@
 # You should have received a copy of the GNU General Public License along with this project. If
 # not, see <http://www.gnu.org/licenses/>.
 
-
 from django import template
+from django.core.urlresolvers import reverse
+from django.utils.html import format_html
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
 def render(context, variable):
-    variable = '{%% load blog %%}%s' % variable
+    variable = '{%% load blog render %%}%s' % variable
     t = template.Template(variable)
     return t.render(context)
+
+
+@register.simple_tag
+def path(urlname, text, alt=None, anchor=None, **kwargs):
+    path = reverse(urlname, kwargs=kwargs)
+    if anchor is not None:
+        path = '%s#%s' % (path, anchor)
+
+    alt = alt or text
+
+    return format_html('<a href="{}" alt="{}">{}</a>', path, alt, text)
