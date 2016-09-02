4430a895f20c93516d612f42bf6337f1a2fce1da
Mathias Ertl <mati@er.tl>
2016-09-02 17:05:45 +0200

    add dnsbl function

---
+++ hp/core/utils.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the jabber.at homepage (https://github.com/jabber-at/hp).
+#
+# This project is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-xmpp-account.
+# If not, see <http://www.gnu.org/licenses
+
+import dns.resolver
+
+from django.conf import settings
+
+
+def check_dnsbl(ip):
+    blocks = []
+    for dnsbl in settings.DNSBL:
+        print('checking %s' % dnsbl)
+        reason = None
+        resolver = dns.resolver.Resolver()
+        query = '.'.join(reversed(str(ip).split("."))) + "." + dnsbl
+
+        try:
+            answers = resolver.query(query, "A")
+        except dns.resolver.NXDOMAIN:  # not blacklisted
+            continue
+
+        try:
+            reason = resolver.query(query, "TXT")[0]
+        except:  # reason is optional
+            pass
+
+        blocks.append((answers[0], reason))
+
+    return blocks
+
