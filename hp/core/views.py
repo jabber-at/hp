65a58946cf50cb60118dfbba2f83b4afa460a2a5
Mathias Ertl <mati@er.tl>
2016-09-03 15:33:14 +0200

    finish the RateLimitMixin

--- hp/core/views.py
+++ hp/core/views.py
@@ -36,6 +36,9 @@ from .models import Page
 from .models import BlogPost
 from .utils import check_dnsbl
 
+_RATELIMIT_WHITELIST = getattr(settings, 'RATELIMIT_WHITELIST', set())
+_RATELIMIT_CONFIG = getattr(settings, 'RATELIMIT_CONFIG', {})
+
 
 class TranslateSlugViewMixin(object):
     """A view mixin that allows DetailView to work with translated slugs.
@@ -133,16 +136,37 @@ class RateLimitMixin(object):
         return 'rate_%s_%s' % (self.rate_activity, request.META['REMOTE_ADDR'])
 
     def check_rate(self, request):
-        if settings.DEBUG is True:
-            return True
-        pass
+#        if request.META['REMOTE_ADDR'] in _RATELIMIT_WHITELIST or settings.DEBUG is True:
+#            return True
 
-    def add_rated_activity(self, request):
+        cache_key = self.get_rate_cache_key(request)
+        now = timezone.now()
+        timestamps = cache.get(cache_key) or []
+        config = _RATELIMIT_CONFIG.get(self.rate_activity)
+
+        for delta, ratelimit in config:
+            offset = now - delta
+            print('checking delta: %s, ratelimit: %s' % (delta, ratelimit))
+            print('Found: %s' % len([t for t in timestamps if t > offset]))
+            if len([t for t in timestamps if t > offset]) > ratelimit:
+                return False
+        return True
+
+    def dispatch(self, request, *args, **kwargs):
+        print('dispatch...')
+        if self.check_rate(request) is False:
+            return TemplateResponse(request, self.rate_template, {})
+        return super(RateLimitMixin, self).dispatch(request, *args, **kwargs)
+
+    def ratelimit(self, request):
+        if request.META['REMOTE_ADDR'] in _RATELIMIT_WHITELIST:
+            return
+
         cache_key = self.get_rate_cache_key(request)
         now = timezone.now()
         timestamps = cache.get(cache_key) or []
         timestamps.append(now)
-        cache.set(timestamps)
+        cache.set(cache_key, timestamps, timeout=86400)
 
 
 class PageView(TranslateSlugViewMixin, DetailView):
