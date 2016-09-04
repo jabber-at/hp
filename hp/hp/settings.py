1951d7f47b4047d356ea6bfe4a2b0d6fbd140088
Mathias Ertl <mati@er.tl>
2016-09-04 12:48:30 +0200

    add more settings

--- hp/hp/settings.py
+++ hp/hp/settings.py
@@ -146,6 +146,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
