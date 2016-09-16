555ea492a9d495c53f377e6e2f6f63129e859da0
Mathias Ertl <mati@er.tl>
2016-09-16 15:21:56 +0200

    enable RTD theme

--- doc/conf.py
+++ doc/conf.py
@@ -349,3 +349,8 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+# Theme config
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
