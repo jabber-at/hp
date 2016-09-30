15f656284b01300cd3ab29ab8b0423f31f12f99f
Mathias Ertl <mati@er.tl>
2016-09-30 19:18:44 +0200

    filter client list on startup

--- hp/core/static/core/js/clients.js
+++ hp/core/static/core/js/clients.js
@@ -1,13 +1,18 @@
+var update_client_list = function(elem) {
+    var value = elem.val();
+    if (value == "any") {
+        $('table#clients-table tr').show();
+    } else {
+        $('table#clients-table tr').hide();
+        $('table#clients-table tr.header-row').show();
+        $('table#clients-table tr.' + value).show();
+    }
+};
+
 $(document).ready(function() {
+    update_client_list($('form#clients-form select#id_os'));
+
     $('form#clients-form select#id_os').change(function(e) {
-        var value = $(e.target).val();
-        console.log('changing: ' + value);
-        if (value == "any") {
-            $('table#clients-table tr').show();
-        } else {
-            $('table#clients-table tr').hide();
-            $('table#clients-table tr.header-row').show();
-            $('table#clients-table tr.' + value).show();
-        }
+        update_client_list($(e.target));
     });
 });
