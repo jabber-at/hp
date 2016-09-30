1e03e24b8ad5ab68c52f51a2629bb677286246ef
Mathias Ertl <mati@er.tl>
2016-09-30 10:09:03 +0200

    fix bosh url

--- hp/conversejs/static/conversejs/init.js
+++ hp/conversejs/static/conversejs/init.js
@@ -1,6 +1,6 @@
 require(['converse'], function (converse) {
     converse.initialize({
-        bosh_service_url: 'https://http.jabber.at/http-bind', // Please use this connection manager only for testing purposes
+        bosh_service_url: 'https://http.jabber.at/http-bind/', // Please use this connection manager only for testing purposes
         i18n: locales.en, // Refer to ./locale/locales.js to see which locales are supported
         show_controlbox_by_default: true,
         roster_groups: true,
