b7c4ace48eaae29f8f1425e75d0fe1854d48c235
Mathias Ertl <mati@er.tl>
2016-09-30 10:02:34 +0200

    do not allow registration

--- hp/conversejs/static/conversejs/init.js
+++ hp/conversejs/static/conversejs/init.js
@@ -3,6 +3,7 @@ require(['converse'], function (converse) {
         bosh_service_url: 'https://http.jabber.at/http-bind', // Please use this connection manager only for testing purposes
         i18n: locales.en, // Refer to ./locale/locales.js to see which locales are supported
         show_controlbox_by_default: true,
-        roster_groups: true
+        roster_groups: true,
+        allow_registration: false
     });
 });
