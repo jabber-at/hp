a9a95aeffe08b5e71adc6ff44cd3474312996a3b
Mathias Ertl <mati@er.tl>
2016-10-14 19:58:43 +0200

    add basic js/css for notifications

---
+++ hp/account/static/account/js/notifications.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('form.user-notifications input[type="checkbox"]').change(function(event) {
+        var checkbox = $(event.currentTarget);
+        var form = $('form.user-notifications');
+        console.log(form);
+
+        $.ajax({
+            type: form.attr('method'),
+            url: form.attr('action'),
+            data: form.serialize(),
+            success: function(data) {
+                checkbox.parent('label').find('.glyphicon').fadeIn(150).delay(500).fadeOut(150);
+            },
+            error: function(data) {
+            }
+        });
+    });
+});
