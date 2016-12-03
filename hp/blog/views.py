3d439f0279ecb407c37bdedb4b0f07f3faf5b1b1
Mathias Ertl <mati@er.tl>
2016-12-03 12:26:15 +0100

    do not format with site (they are templates now anyway)

--- hp/blog/views.py
+++ hp/blog/views.py
@@ -35,14 +35,14 @@ class BasePageMixin(object):
     def get_context_data(self, object, **kwargs):
         # TODO: og_type (article/website)
         context = super(BasePageMixin, self).get_context_data(**kwargs)
-        context['meta_desc'] = object.get_meta_summary(self.request) % self.request.site
+        context['meta_desc'] = object.get_meta_summary(self.request)
         context['updated'] = object.updated
         context['created'] = object.created
         context['og_title'] = object.title.current % self.request.site
-        context['og_desc'] = object.get_opengraph_summary(self.request) % self.request.site
+        context['og_desc'] = object.get_opengraph_summary(self.request)
         context['canonical_url'] = object.get_canonical_url()
         context['twitter_title'] = object.title.current % self.request.site
-        context['twitter_desc'] = object.get_twitter_summary(self.request) % self.request.site
+        context['twitter_desc'] = object.get_twitter_summary(self.request)
         context['title'] = 'foo'
         return context
 
