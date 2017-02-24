b83c7be1ef84aef6ed56e927f316d866cd3294c9
Mathias Ertl <mati@er.tl>
2017-02-24 08:06:06 +0100

    add stats constants

---
+++ hp/stats/constants.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the jabber.at homepage (https://github.com/jabber-at/hp).
+#
+# This project is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-xmpp-account.
+# If not, see <http://www.gnu.org/licenses/>.
+
+STAT_REGISTER = 0
+STAT_REGISTER_CONFIRMED = 1
+STAT_RESET_PASSWORD = 2
+STAT_RESET_PASSWORD_CONFIRMED = 3
+STAT_SET_PASSWORD = 4
+STAT_SET_EMAIL = 5
+STAT_SET_EMAIL_CONFIRMED = 6
+STAT_FAILED_LOGIN = 7
