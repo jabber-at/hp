cb2488aa5471bc156b7a4946c25d37623a50b4d8
Mathias Ertl <mati@er.tl>
2017-03-11 17:25:00 +0100

    finally add first tests by using doctests

--- hp/core/tests.py
+++ hp/core/tests.py
@@ -1,3 +1,23 @@
-# from django.test import TestCase
+# -*- coding: utf-8 -*-
+#
+# This file is part of the jabber.at homepage (https://github.com/jabber-at/hp).
+#
+# This project is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with django-xmpp-account.
+# If not, see <http://www.gnu.org/licenses/>.
 
-# Create your tests here.
+import doctest
+
+from . import utils
+
+
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(utils))
+    return tests
