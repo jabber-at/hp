9526a2c5ea3543f6a4b40af3ffdd2ef02be6cffb
Mathias Ertl <mati@er.tl>
2017-04-29 14:03:39 +0200

    remove static init.js

--- hp/conversejs/static/conversejs/init.js
+++
@@ -1,9 +0,0 @@
-//require(['converse'], function (converse) {
-    converse.initialize({
-        bosh_service_url: 'https://jabber.at/http-bind/', // Please use this connection manager only for testing purposes
-        i18n: locales.en, // Refer to ./locale/locales.js to see which locales are supported
-        show_controlbox_by_default: true,
-        roster_groups: true,
-        allow_registration: false
-    });
-//});
