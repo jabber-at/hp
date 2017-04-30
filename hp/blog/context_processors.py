746758327c12f6b1dc69b9832a1af10998fdb6ad
Mathias Ertl <mati@er.tl>
2017-04-30 13:16:07 +0200

    catch misconfiguration and log errors

--- hp/blog/context_processors.py
+++ hp/blog/context_processors.py
@@ -13,10 +13,14 @@
 # You should have received a copy of the GNU General Public License along with django-xmpp-account. If not,
 # see <http://www.gnu.org/licenses/.
 
+import logging
+
 from django.conf import settings
 from django.core.cache import cache
 
 from .models import Page
+
+log = logging.getLogger(__name__)
 
 
 def global_pages(request):
@@ -31,12 +35,20 @@ def global_pages(request):
     if ctx is None:
         ctx = {}
         if settings.CLIENTS_PAGE is not None:
-            page = Page.objects.pk_or_slug(settings.CLIENTS_PAGE)
-            ctx['CLIENTS_URL'] = page.get_absolute_url()
+            try:
+                page = Page.objects.pk_or_slug(settings.CLIENTS_PAGE)
+            except Page.DoesNotExist:
+                log.error('CLIENTS_PAGE "%s" does not exist.', settings.CLIENTS_PAGE)
+            finally:
+                ctx['CLIENTS_URL'] = page.get_absolute_url()
 
         if settings.FAQ_PAGE is not None:
-            page = Page.objects.pk_or_slug(settings.FAQ_PAGE)
-            ctx['FAQ_URL'] = page.get_absolute_url()
+            try:
+                page = Page.objects.pk_or_slug(settings.FAQ_PAGE)
+            except Page.DoesNotExist:
+                log.error('FAQ_PAGE "%s" does not exist.', settings.FAQ_PAGE)
+            finally:
+                ctx['FAQ_URL'] = page.get_absolute_url()
 
         cache.set(cache_key, ctx)
 
