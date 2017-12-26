d2ea25ced68e419c1c81c88d6f6013f7c91d3be3
Mathias Ertl <mati@er.tl>
2017-12-26 22:35:02 +0100

    add some basic test cases for Django

---
+++ hp/core/tests/base.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the jabber.at homepage (https://github.com/jabber-at/hp).
+#
+# This project is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along with this project. If not, see
+# <http://www.gnu.org/licenses/>.
+
+from contextlib import contextmanager
+from unittest import mock
+
+from celery import task
+
+from django.test import TestCase as DjangoTestCase
+
+
+class HomepageTestCaseMixin(object):
+    def assertIsTask(self, t, expected):
+        self.assertEqual(t, task(expected))
+
+    def assertTaskCall(self, mocked, task, *args, **kwargs):
+        self.assertTrue(mocked.called)
+        a, k = mocked.call_args
+        self.assertEqual(k, {})  # apply_async receives task args/kwargs as tuple/dict arg
+
+        instance, called_args, called_kwargs = a
+
+        self.assertIsTask(instance, task)
+        self.assertEqual(args, called_args)
+        self.assertEqual(kwargs, called_kwargs)
+
+    @contextmanager
+    def mock_celery(self):
+        def run(self, args, kwargs):
+            return self.run(*args, **kwargs)
+
+        with mock.patch('celery.app.task.Task.apply_async', side_effect=run, autospec=True) as func:
+            yield func
+
+
+class TestCase(HomepageTestCaseMixin, DjangoTestCase):
+    pass
---
+++ hp/core/tests/tests_urlpatterns.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the jabber.at homepage (https://github.com/jabber-at/hp).
+#
+# This project is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# This project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along with this project. If not, see
+# <http://www.gnu.org/licenses/>.
+
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls import url
+from django.test import Client
+from django.test import override_settings
+from django.utils.translation import ugettext_lazy as _
+
+from .. import views
+from ..urlpatterns import i18n_re_path
+from .base import TestCase
+
+app_name = 'core'
+urlpatterns = [
+    i18n_re_path(_(r'^contact/$'), views.ContactView.as_view(), name='contact'),
+
+    # URLs provided by these modules are used in the base template, so we have to include them here too
+    url(r'^captcha/', include('captcha.urls')),
+    url(r'^account/', include('account.urls')),
+    url(r'^feed/', include('feed.urls')),
+    url(r'^', include('blog.urls')),
+    url(r'^', include('core.urls')),
+]
+
+
+@override_settings(ROOT_URLCONF=__name__)
+class I18nURLTests(TestCase):
+    def test_re_path(self):
+        c = Client()
+        with self.assertTemplateUsed('core/contact.html'):
+            response = c.get('/contact/')
+            self.assertEqual(response.status_code, 200)
+
+        response = c.get('/kontakt/')
+        self.assertRedirects(response, '/contact/')
+
+        # Set language to German and test new redirects
+        c.cookies.load({settings.LANGUAGE_COOKIE_NAME: 'de'})
+        with self.assertTemplateUsed('core/contact.html'):
+            response = c.get('/kontakt/')
+            self.assertEqual(response.status_code, 200)
+
+        response = c.get('/contact/')
+        self.assertRedirects(response, '/kontakt/')
+
+        # Set language to French - translated URLs redirect to English version
+        c.cookies.load({settings.LANGUAGE_COOKIE_NAME: 'fr'})
+        response = c.get('/kontakt/')
+        self.assertRedirects(response, '/contact/')
+
+        with self.assertTemplateUsed('core/contact.html'):
+            response = c.get('/contact/')
+            self.assertEqual(response.status_code, 200)
