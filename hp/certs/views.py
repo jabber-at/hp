36d7331a94cc14c5744326c1ae5c7d50c6cbc0ca
Mathias Ertl <mati@er.tl>
2018-03-03 09:34:07 +0100

    return queryset

--- hp/certs/views.py
+++ hp/certs/views.py
@@ -13,6 +13,7 @@
 # You should have received a copy of the GNU General Public License along with this project. If not, see
 # <http://www.gnu.org/licenses/>.
 
+from django.conf import settings
 from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
@@ -24,7 +25,21 @@ from .models import Certificate
 class CertificateOverview(ListView):
     """List all available hostnames and the last update."""
 
-    queryset = Certificate.objects.newest()
+    queryset = Certificate.objects.all()
+    template_name = 'certs/certificate_list.html'
+
+    def get_queryset(self):
+        qs = super(CertificateOverview, self).get_queryset()
+        certs = []
+
+        for hostname in settings.XMPP_HOSTS.keys():
+            # first we get the newest valid one
+            cert = qs.filter(hostname=hostname).valid().first()
+
+            if cert:
+                certs.append(cert)
+
+        return certs
 
 
 class CertificateView(DetailView):
@@ -40,7 +55,7 @@ class CertificateView(DetailView):
         if 'date' in self.kwargs:
             queryset = queryset.filter(valid_from__date=self.kwargs['date'])
         else:
-            queryset = queryset.order_by('-created')
+            queryset = queryset.order_by('-valid_from')
 
         obj = queryset.first()
         if obj is None:
