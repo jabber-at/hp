9cb42169fe5c10b316b006dc0b2783140907f42e
Mathias Ertl <mati@er.tl>
2018-03-30 19:43:35 +0200

    set update handling of error codes

--- hp/account/static/account/js/username_widget.js
+++ hp/account/static/account/js/username_widget.js
@@ -1,91 +1,46 @@
-var check_username = function(input, timer) {
-    let form_group = input.parents('.form-group.form-group-username');
+var check_username = function(form_group, timer) {
+    let input = form_group.find('input#id_username_0');
     let domain_select = form_group.find('select#id_username_1');
 
-    /** mark input as valid. */
-    let mark_valid = function() {
-        form_group.addClass('fg-valid');
-        form_group.removeClass('fg-invalid-syntax');
-        form_group.removeClass('fg-invalid-exists');
-        form_group.removeClass('fg-invalid-error');
+    clearTimeout(timer);
 
-        input[0].setCustomValidity('');
-        domain_select[0].setCustomValidity('');
-    }
+    if (input[0].checkValidity()) {
+        timer = setTimeout(function() {
+            let exists_url = $('meta[name="account:api-check-user"]').attr('content');
 
-    /** mark input as invalid because of it's syntax (too short/long, invalid chars) */
-    let invalid_syntax = function() {
-        form_group.addClass('fg-invalid-syntax');
-        form_group.removeClass('fg-valid');
-        form_group.removeClass('fg-invalid-exists');
-        form_group.removeClass('fg-invalid-error');
+            $.post(exists_url, {
+                username: input.val(),
+                domain: domain_select.val()
+            }).done(function(data) { // user does not exist yet
+                input[0].setCustomValidity("");
+                domain_select[0].setCustomValidity("");
+            }).fail(function(data) {
+                if (data.status == 409) {  // 409 = HTTP conflict -> The user already exists.
+                    error = bs4_forms_set_error(input, 'unique');
+                    domain_select[0].setCustomValidity(error.message);
+                } else {
+                    bs4_forms_set_error(input, 'error');
 
-        let invalid_msg = form_group.find('.invalid-exists').text().trim();
-        domain_select[0].setCustomValidity(invalid_msg);
-    }
-
-    let minlength = parseInt(input.attr('minlength'), 10);
-    let maxlength = parseInt(input.attr('maxlength'), 10);
-    let value = input.val();
-
-    if (value.length < minlength || value.length > maxlength) {
-        invalid_syntax();
-        return;
-    }
-
-    let check_existance = input.data('check-existance');
-    input[0].setCustomValidity('');
-
-    if (check_existance) {
-        clearTimeout(timer);
-
-        if (input[0].checkValidity()) {
-            timer = setTimeout(function() {
-                let domain = form_group.find('select#id_username_1 option:selected').val();
-                let exists_url = $('meta[name="account:api-check-user"]').attr('content');
-
-                $.post(exists_url, {
-                    username: value,
-                    domain: domain
-                }).done(function(data) { // user exists!
-                    mark_valid();
-                }).fail(function(data) {
-                    if (data.status == 409) {  // 409 = HTTP conflict -> The user already exists.
-                        let exists_msg = form_group.find('.invalid-exists').text().trim();
-                        input[0].setCustomValidity(exists_msg);
-                        domain_select[0].setCustomValidity(exists_msg);
-                        
-                        form_group.addClass('fg-invalid-exists');
-                        form_group.removeClass('fg-valid');
-                    } else {
-                        form_group.addClass('fg-invalid-error');
-                        form_group.removeClass('fg-valid');
-
-                        let error_msg = form_group.find('.invalid-error').text().trim();
-                        input[0].setCustomValidity(error_msg);
-                        domain_select[0].setCustomValidity(error_msg);
-                    }
-                });
-            }, 100);
-        } else {
-            invalid_syntax();
-        }
+                    domain_select[0].setCustomValidity(error_msg);
+                }
+            });
+        }, 100);
     } else {
-        if (input[0].checkValidity()) {
-            mark_valid();
-        } else {
-            invalid_syntax();
-        }
+        invalid_syntax();
     }
 };
 
 $(document).ready(function() {
     var username_timer;
 
-    $('#id_username_0').on('input propertychange paste', function(e) {
-        check_username($(e.target), username_timer);
+    $('input#id_username_0[data-check-existance="true"]').on('input propertychange paste', function(e) {
+        let form_group = $(e.target).parents('.form-group');
+        if (e.target.checkValidity()) {  // only check existance if input is valid
+            check_username(form_group, username_timer);
+        }
     });
     $('#id_username_1').change(function(e) {
-        check_username($(e.target), username_timer);
+        let form_group = $(e.target).parents('.form-group');
+        check_username(form_group, username_timer);
     });
 });
--- hp/bootstrap/static/bootstrap/js/bootstrap.js
+++ hp/bootstrap/static/bootstrap/js/bootstrap.js
@@ -1,3 +1,25 @@
+var bs4_forms_set_error = function(elem, cls) {
+    let cls_name = 'invalid-' + cls;
+    let form_group = elem.parents('.form-group');
+    
+    form_group.addClass(cls_name);
+    let message = form_group.find('.invalid-feedback.' + cls_name).text();
+    message = message ? message.trim() : null;
+    if (message) {
+        elem[0].setCustomValidity(message);
+    }
+
+    return {
+        message: message
+    }
+};
+
+var bs4_forms_clear_error = function(form_group) {
+    form_group.removeClass(function(index, className) {
+        return (className.match (/(^|\s)invalid-\S+/g) || []).join(' ');
+    });
+};
+
 $(document).ready(function() {
     /* Disable built-in form validation and use javascript. */
     $('form').attr('novalidate', true);
@@ -15,6 +37,27 @@ $(document).ready(function() {
 
         if (typeof min_length === 'undefined' || value.length > min_length) {
             input.parents('.form-group').addClass('was-validated');
+        }
+    });
+
+    $('.form-group input').on('input propertychange paste', function(e) {
+        let input = $(e.target);
+        let value = input.val();
+        let form_group = input.parents('.form-group');
+
+        // first, clear any custom message
+        e.target.setCustomValidity('');
+        bs4_forms_clear_error(form_group);
+
+        // remove any classes
+
+        // if the field is required, display the appropriate message
+        if (! e.target.checkValidity()) {
+            if (value.length == 0 && input.prop('required')) {
+                bs4_forms_set_error(input, 'required');
+            } else {
+                bs4_forms_set_error(input, 'invalid');
+            }
         }
     });
 });
