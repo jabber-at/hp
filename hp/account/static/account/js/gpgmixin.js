f343b0ef818e62c128c9f41c46b33eac650d424b
Mathias Ertl <mati@er.tl>
2018-04-01 12:58:04 +0200

    remove element dep

--- hp/account/static/account/js/gpgmixin.js
+++ hp/account/static/account/js/gpgmixin.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('.gpg-content.hidden-with-js').hide();
-    $('.gpg-header-row.hidden-with-js i.fa-angle-right').show();
-    $('.gpg-header-row.hidden-with-js i.fa-angle-down').hide();
+    $('.gpg-header-row.hidden-with-js .fa-angle-right').show();
+    $('.gpg-header-row.hidden-with-js .fa-angle-down').hide();
 
     $('.gpg-header-row').click(function(data) {
         $('.gpg-content').slideToggle("fast");
