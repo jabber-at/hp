96a620572726752717bac30401399b0cfefea59b
Mathias Ertl <mati@er.tl>
2018-04-02 12:15:04 +0200

    add ability for sume defualt buttons

--- hp/bootstrap/forms.py
+++ hp/bootstrap/forms.py
@@ -13,10 +13,19 @@
 # You should have received a copy of the GNU General Public License along with this project. If
 # not, see <http://www.gnu.org/licenses/>.
 
+from django.utils.translation import ugettext_lazy as _
+
 
 class BootstrapFormMixin(object):
     """Mixin for Bootstrap forms, making (some) variables globally overridable."""
 
+    button_order = ['submit']
+    default_buttons = {
+        'submit': {
+            'text': _('Submit'),
+            'class': 'primary',
+        },
+    }
     input_columns = None
     label_columns = None
     offset_columns = 'col-sm-10 offset-sm-2'
@@ -36,6 +45,16 @@ class BootstrapFormMixin(object):
         if label_columns is not None:
             for key, field in self.fields.items():
                 field.label_columns = label_columns
+
+        buttons = {}
+        for c in reversed(self.__class__.__mro__):
+            buttons.update(getattr(c, 'default_buttons', {}))
+        buttons.update(kwargs.pop('buttons', {}))
+        self.buttons = buttons
+
+    def get_buttons(self):
+        order = self.button_order
+        return sorted(self.buttons.items(), key=lambda t: order.index(t[0]) if t[0] in order else -1)
 
     def get_offset_columns(self):
         return self.offset_columns
