56563edfaa678405d6bbe1a5a4f9f3b49f4f6742
Mathias Ertl <mati@er.tl>
2020-03-22 17:40:01 +0100

    remove unused STATICFILES_DIRS

--- hp/hp/settings.py
+++ hp/hp/settings.py
@@ -165,9 +165,6 @@ USE_TZ = True
 
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 AUTH_USER_MODEL = 'account.User'
 LOGIN_URL = reverse_lazy('account:login')
 LOGIN_REDIRECT_URL = reverse_lazy('account:detail')
