639a206accbdfd323fc2cbfeb35d0e367af5da19
Mathias Ertl <mati@er.tl>
2020-04-05 18:26:48 +0200

    do not filter output (breaks nc for some reason)

--- uwsgi.sh
+++ uwsgi.sh
@@ -3,7 +3,7 @@
 DATABASE_HOST=${DATABASE_HOST:-db}
 DATABASE_PORT=${DATABASE_PORT:-5432}
 
-while ! nc -z ${DATABASE_HOST} ${DATABASE_PORT} &> /dev/null; do
+while ! nc -z ${DATABASE_HOST} ${DATABASE_PORT}; do
     echo "Wait for ${DATABASE_HOST}:${DATABASE_PORT}..."
     sleep 0.1 # wait for 1/10 of the second before check again
 done
